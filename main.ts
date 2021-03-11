74e07d98a2057df0337edd0bf638497c3f551faa
kelly.li <60376946+Kelliyinghua@users.noreply.github.com>
2021-03-11 13:24:10 -0500

    Added Temperature

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
+let tempt = 0
 let degrees = 0
 basic.forever(function () {
+    tempt = input.temperature()
     degrees = input.compassHeading()
-    if (degrees < 45 || degrees >= 315) {
-        basic.showString("N")
-    } else if (degrees < 135) {
-        basic.showString("E")
-    } else if (degrees < 225) {
-        basic.showString("S")
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showString("" + (tempt))
     } else {
-        basic.showString("W")
+        if (degrees < 45 || degrees >= 315) {
+            basic.showString("N")
+        } else if (degrees < 135) {
+            basic.showString("E")
+        } else if (degrees < 225) {
+            basic.showString("S")
+        } else {
+            basic.showString("W")
+        }
     }
 })
