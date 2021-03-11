a2915bb1dbd36c933861b8a2dcb6d1b5b2c35f18
kelly.li <60376946+Kelliyinghua@users.noreply.github.com>
2021-03-11 14:07:07 -0500

    Added on btn pressed structure and while loop

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
-let tempt = 0
-let degrees = 0
-basic.forever(function () {
-    tempt = input.temperature()
-    degrees = input.compassHeading()
-    if (input.buttonIsPressed(Button.B)) {
-        basic.showString("" + (tempt))
-        basic.pause(500)
-        basic.showIcon(IconNames.Yes)
-    } else if (input.buttonIsPressed(Button.A)) {
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        degrees = input.compassHeading()
         if (degrees < 45 || degrees >= 315) {
             basic.showString("N")
         } else if (degrees < 135) {
@@ -17,7 +10,18 @@ basic.forever(function () {
         } else {
             basic.showString("W")
         }
-    } else {
-        basic.showString("Click A / B")
+        if (input.buttonIsPressed(Button.B)) {
+            break;
+        }
     }
 })
+input.onButtonPressed(Button.B, function () {
+    tempt = input.temperature()
+    basic.clearScreen()
+    basic.showString("" + (tempt))
+    basic.pause(500)
+    basic.showIcon(IconNames.Yes)
+})
+let tempt = 0
+let degrees = 0
+basic.showString("PRESS A/B")
